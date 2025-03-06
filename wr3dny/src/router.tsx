7b4a244db676febbebffa7aac79a01e4c6ba712e
wr3dny <pawel.pawtel@gmail.com>
2025-03-06 11:25:29 +0100

    5/03

--- wr3dny/src/router.tsx
+++ wr3dny/src/router.tsx
@@ -1,17 +1,13 @@
-import { Route, Routes } from "react-router-dom"
-import { PATHS } from "./const/paths"
-import { Home } from "./pages/Home/Home"
-import { Lego } from "./pages/Lego/Lego"
-import { Gallery } from "./pages/Gallery/Gallery"
-import { NotFound } from "./components/NotFound/NotFound"
+import { Route, Routes } from "react-router-dom";
+import { PATHS } from "./const/paths";
+import { Home } from "./pages/Home/Home";
+import { Lego } from "./pages/Lego/Lego";
 
 export const RouterConfig = () => {
-    return (
-            <Routes>
-                <Route path={PATHS.HOME} element={<Home/>}/>
-                <Route path={PATHS.LEGO} element={<Lego/>}/>
-                <Route path={PATHS.GALLERY} element={<Gallery/>}/>       
-                <Route path="*" element={<NotFound />} />      
-            </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path={PATHS.HOME} element={<Home />} />
+      <Route path={PATHS.LEGO} element={<Lego />} />
+    </Routes>
+  );
+};
