d2b7eb4d4ed568300ef72f9b6022fef1f3a6d923
wr3dny <pawel.pawtel@gmail.com>
2025-03-29 20:53:48 +0100

    29/03

--- wr3dny/src/components/Button/Button.tsx
+++ wr3dny/src/components/Button/Button.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "./Button.module.css";
+import classNames from "classnames";
+
+type ButtonType = "black" | "white";
 
 interface Props {
   label: string;
   onClick: () => void;
   icon?: React.ElementType;
+  type: ButtonType;
 }
 
-export const Button = ({ label, onClick, icon: Icon }: Props) => {
+export const Button = ({ label, onClick, icon: Icon, type }: Props) => {
   return (
-    <button onClick={onClick} className={styles.container}>
+    <button
+      onClick={onClick}
+      className={classNames(styles.container, styles[type])}
+    >
       {Icon && <Icon />}
       <span>{label}</span>
     </button>
