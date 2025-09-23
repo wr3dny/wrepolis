5e943230489233829284b59cbc6495e156235468
wr3dny <pawel.pawtel@gmail.com>
2025-09-23 22:55:38 +0200

    little change to navbar

--- wr3dny/src/components/Navbar/Navbar.tsx
+++ wr3dny/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,68 @@
 import { NavLink } from "react-router-dom";
 import { PATHS } from "../../const/paths";
 import styles from "./Navbar.module.css";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className={styles.navbar}>
-      <ul>
-        {Object.entries(PATHS).map(([key, value]) => (
-          <li key={key}>
-            <NavLink to={value} end>
-              {key}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className={styles.shell}>
+      <div className={styles.topBar}>
+        <nav className={styles.navbar}>
+          <div className={styles.logoWrap}>
+            {/* LOGO */}
+            <span className={styles.logo} data-text="NEONNET">
+              NEONNET
+            </span>
+            <span className={styles.badge}>v1.0</span>
+          </div>
+          {/* desktop links */}
+          <ul className={styles.listDesktop}>
+            {Object.entries(PATHS).map(([key, value]) => (
+              <li key={key}>
+                <NavLink
+                  to={value}
+                  end
+                  className={({ isActive }) =>
+                    [styles.link, isActive ? styles.active : ""].join(" ")
+                  }
+                >
+                  <span className={styles.linkFrame} />
+                  <span className={styles.linkCornerTL} />
+                  <span className={styles.linkCornerBR} />
+                  {key}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {/* Mobile panel */}
+        <div
+          className={[styles.mobilePanel, open ? styles.open : ""].join(" ")}
+        >
+          <ul className={styles.listMobile}>
+            {Object.entries(PATHS).map(([key, value]) => (
+              <li key={key} className={styles.itemMobile}>
+                <NavLink
+                  to={value}
+                  end
+                  onClick={() => setOpen(false)}
+                  className={({ isActive }) =>
+                    [
+                      styles.mobileLink,
+                      isActive ? styles.mobileActive : "",
+                    ].join(" ")
+                  }
+                >
+                  {key}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
